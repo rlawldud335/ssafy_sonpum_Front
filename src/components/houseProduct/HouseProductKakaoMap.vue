80d0e86b6a6ba7526220810b2b08506480a2070a
Halo <87004079+Halo-oo@users.noreply.github.com>
2022-11-24 15:50:09 +0900

    ADD: kakaoMap 컨트롤러 추가

--- src/components/houseProduct/HouseProductKakaoMap.vue
+++ src/components/houseProduct/HouseProductKakaoMap.vue
@@ -89,6 +89,17 @@ export default {
       //지도 객체를 등록합니다.
       //지도 객체는 반응형 관리 대상이 아니므로 initMap에서 선언합니다.
       this.map = new kakao.maps.Map(container, options);
+
+      // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+      var mapTypeControl = new kakao.maps.MapTypeControl();
+
+      // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+      // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+      this.map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+      // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+      var zoomControl = new kakao.maps.ZoomControl();
+      this.map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     },
     changeSize(size) {
       const container = document.getElementById("map");
